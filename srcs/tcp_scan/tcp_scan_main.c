b510a963a9150b42bbbf4342901636642f56452a
Hugo KOVAC <hkovac@paul-f4Br7s4.clusters.42paris.fr>
2023-08-09 23:13:01 +0200

    create threads

--- srcs/tcp_scan/tcp_scan_main.c
+++ srcs/tcp_scan/tcp_scan_main.c
@@ -2,14 +2,38 @@
 #include "../includes/includes.h"
 #include "../includes/define.h"
 
+struct thread_info {
+	pthread_t thread_id;
+	int thread_num;
+	char *argv_string;
+};
+
+void *start_thread(void *arg){
+	(void)arg;
+
+	printf("ok");
+
+	return NULL;
+}
+
 int tcp_tester(t_context *context)
 {
 	struct socket_info *sockets_info = init_list(context);
 	if (!sockets_info)
 		return 1;
 	struct socket_info *sockets_info_cpy = sockets_info;
-
 	sockets_info_cpy = sockets_info;
+
+	// int hosts_count = count_hostnames(context->hostnames);
+	struct thread_info *tinfo = ft_calloc(context->thread_count, sizeof(struct thread_info));
+	if (!tinfo){
+		clean_list(sockets_info);
+			return 4;
+	}
+
+	for (int i = 0; i < context->thread_count; i++)
+		pthread_create(&tinfo[i].thread_id, NULL, start_thread, NULL);
+
 	for (int j = 0; context->hostnames[j]; j++)
 	{
 		for (int k = 0; k < context->port_count; k++, sockets_info_cpy = sockets_info_cpy->next)
@@ -18,7 +42,7 @@ int tcp_tester(t_context *context)
 				clean_list(sockets_info);
 				return 2;
 			}
-			for (int i = 0; i < SCAN_COUNT - 1; i++){//! -1 cause of UDP
+			for (int i = 0; i < SCAN_COUNT - 1; i++){// -1 cause of UDP
 				if (!context->scan_types[i]){
 					continue;
 				}
@@ -38,6 +62,10 @@ int tcp_tester(t_context *context)
 			clean_in_thread(sockets_info_cpy);
 		}
 	}
+
+	for (int i = 0; i < context->thread_count; i++)
+		pthread_join(tinfo[i].thread_id, NULL);
+	free(tinfo);
 
 	clean_list(sockets_info);
 	return 0;
